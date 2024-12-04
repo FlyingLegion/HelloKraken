5814804c6fd0cf06e1f014108e4d3f84bf5823c5
Flying Legion <hcrt@3635@gmail.com>
2024-12-04 14:22:26 -0900

    Test Message I hate you

--- KrakenMotor/src/main/java/frc/robot/Robot.java
+++ KrakenMotor/src/main/java/frc/robot/Robot.java
@@ -81,7 +81,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    System.out.println("I hate Github OMG");
+  }
 
   @Override
   public void testInit() {
